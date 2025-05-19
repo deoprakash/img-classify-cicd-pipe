06eb0df3d775bf75c1c0e7afff9b41501b50eec6
DEO PRAKASH <deo.prakash@ssipmt.com>
2025-05-19 16:10:13 +0530

    updates app.py

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,6 @@ from PIL import Image
 import numpy as np
 
 def preprocess_image(file):
-    img = Image.open(file).resize(32, 32)
+    img = Image.open(file).resize((32, 32), resample=Image.Resampling.LANCZOS)
     img_arr = np.array(img)/ 255.0
     return img_arr
