41959851b7b12a36a1c7274b2fad4533708b495a
DEO PRAKASH <deo.prakash@ssipmt.com>
2025-05-19 16:18:17 +0530

    updates app.py

--- test_app.py
+++ test_app.py
@@ -1,13 +1,13 @@
-# test_app.py
 import requests
 
 def test_prediction():
     url = "http://localhost:5000/predict"
-    with open("data/sample.jpg", "rb") as f:
-        files = {"image": f}
-        response = requests.post(url, files=files)
-        assert response.status_code == 200
-        assert "prediction" in response.json()
+    files = {'image': open('data/sample.jpg', 'rb')}
+    response = requests.post(url, files=files)
+    
+    print("Status Code:", response.status_code)
+    print("Response Text:", response.text)
+    
+    assert response.status_code == 200
 
-if __name__ == "__main__":
-    test_prediction()
+test_prediction()
